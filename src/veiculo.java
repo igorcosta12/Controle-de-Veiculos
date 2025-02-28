7b79f6552242c37e68037b61d1bee8adc60250a5
Igor Costa <89418144+igorcosta12@users.noreply.github.com>
2025-02-28 17:24:27 -0300

    Add files via upload

---
+++ src/veiculo.java
@@ -0,0 +1,25 @@
+public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int capacidadePassageiros;
+    private String combustivel;
+
+    public Veiculo(String marca, String modelo, int ano, int capacidadePassageiros, String combustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.capacidadePassageiros = capacidadePassageiros;
+        this.combustivel = combustivel;
+    }
+
+    public abstract double calcularAutonomia();
+
+    public void exibirDetalhes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("Capacidade de Passageiros: " + capacidadePassageiros);
+        System.out.println("Combustível: " + combustivel);
+    }
+}
